5a2cc7eec973d27f7aa3188c833ed053f2ff2430
Oksana Semenkova <semenkova.oksana.work@gmail.com>
2022-05-03 18:08:07 +0300

    User Defined Variables were added

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("/home/osemenkova/gcp/osem-2504-38ba4ea91eb8.json")
+  credentials = file(var.credentials_file)
 
-  project = "osem-2504"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project = "osem-2504"
+credentials_file = "/home/osemenkova/gcp/osem-2504-38ba4ea91eb8.json"
